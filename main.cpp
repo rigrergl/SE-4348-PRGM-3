32885d3f02e113a15c8895ec61e219a3cbc1a145
Your Name <rigrergl@gmail.com>
2021-11-19 14:58:43 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <chrono>
+#include <thread>
+#include <stdlib.h> /* srand, rand */
+
+int main(int argc, char *argv[])
+{
+    printf("Hello from main");
+}
