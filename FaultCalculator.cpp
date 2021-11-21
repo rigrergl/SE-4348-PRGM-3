88882e90f265dabe77762cb61449fd3781d946c3
Your Name <rigrergl@gmail.com>
2021-11-21 05:07:44 -0600

    fixed clock algorithm

--- FaultCalculator.cpp
+++ FaultCalculator.cpp
@@ -1,6 +1,11 @@
 #include "FaultCalculator.h"
 #include <bits/stdc++.h>
 
+#include <iostream>
+#include <cstring>
+#include <sstream>
+using namespace std;
+
 int LRU(int workingSetSize, int trace[1000])
 {
     int pageFaults = 0;
@@ -73,44 +78,94 @@ int FIFO(int workingSetSize, int trace[1000])
     return pageFaults;
 }
 
-int Clock(int workingSetSize, int trace[1000])
+static bool findAndUpdate(int x, int arr[],
+                          bool second_chance[], int frames)
+
 {
-    int pageFaults = 0;
-    std::unordered_set<int> currentPages;
-    std::unordered_map<int, bool> pageBits;
+    int i;
 
-    for (int i = 0; i < 1000; i++) //trace loop
+    for (i = 0; i < frames; i++)
     {
-        if (currentPages.size() >= workingSetSize &&
-            currentPages.find(trace[i]) == currentPages.end()) //we must remove a page
+
+        if (arr[i] == x)
         {
-            //iterate through currentPages until you find a page with a bit of 0. Remove that page
-            pageFaults++;
+            // Mark that the page deserves a second chance
+            second_chance[i] = true;
 
-            bool foundPageToRemove = false;
-            for (auto pagesIterator = currentPages.begin(); pagesIterator != currentPages.end(); pagesIterator++)
-            {
-                if (!pageBits[*pagesIterator]) //current page has bit = 0
-                {
-                    currentPages.erase(*pagesIterator);
-                    foundPageToRemove = true;
-                }
-                else 
-                {
-                    pageBits[*pagesIterator] = false;
-                }
-            }
-
-            if ( !foundPageToRemove )
-            {
-                auto pagesIterator = currentPages.begin();
-                currentPages.erase(*pagesIterator);
-            }
+            // Return 'true', that is there was a hit
+            // and so there's no need to replace any page
+            return true;
         }
-
-        currentPages.insert(trace[i]);
-        pageBits[trace[i]] = 1;
     }
 
-    return pageFaults;
+    // Return 'false' so that a page for replacement is selected
+    // as he reuested page doesn't exist in memory
+    return false;
+}
+
+// Updates the page in memory and returns the pointer
+static int replaceAndUpdate(int x, int arr[],
+                            bool second_chance[], int frames, int pointer)
+{
+    while (true)
+    {
+
+        // We found the page to replace
+        if (!second_chance[pointer])
+        {
+            // Replace with new page
+            arr[pointer] = x;
+
+            // Return updated pointer
+            return (pointer + 1) % frames;
+        }
+
+        // Mark it 'false' as it got one chance
+        // and will be replaced next time unless accessed again
+        second_chance[pointer] = false;
+
+        //Pointer is updated in round robin manner
+        pointer = (pointer + 1) % frames;
+    }
+}
+
+int Clock(int frames, int trace[])
+{
+    int pointer, i, l = 1000, x, pf;
+
+    //initially we consider frame 0 is to be replaced
+    pointer = 0;
+
+    //number of page faults
+    pf = 0;
+
+    // Create a array to hold page numbers
+    int arr[frames];
+
+    // No pages initially in frame,
+    // which is indicated by -1
+    memset(arr, -1, sizeof(arr));
+
+    // Create second chance array.
+    // Can also be a byte array for optimizing memory
+    bool second_chance[frames];
+
+    for (i = 0; i < l; i++)
+    {
+        x = trace[i];
+
+        // Finds if there exists a need to replace
+        // any page at all
+        if (!findAndUpdate(x, arr, second_chance, frames))
+        {
+            // Selects and updates a victim page
+            pointer = replaceAndUpdate(x, arr,
+                                       second_chance, frames, pointer);
+
+            // Update page faults
+            pf++;
+        }
+    }
+
+    return pf;
 }
